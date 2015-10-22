515048fb9bd08646800b357535900820585d398a
Michael Moss <realmike33@gmail.com>
2015-10-21 19:42:33 -0700

    Initial Commit

---
+++ safariNG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  safariNG
+//
+//  Created by Michael Moss on 10/21/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ safariNG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  safariNG
+//
+//  Created by Michael Moss on 10/21/15.
+//  Copyright © 2015 Mike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
